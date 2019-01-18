3a5357c58faba2411dec66cf4f241da9b8da21d0
Julien Bruguet <lineproex@gmail.com>
2019-01-18 14:51:37 +0100

    Some optimisation

--- php/plusOne.php
+++ php/plusOne.php
@@ -2,13 +2,6 @@
 session_start();
 
 include 'database_connection.php';
-/*
-$id = $_POST["id"];
-
-$rqt = $databaseConnection->prepare('UPDATE messages SET plusone = plusone + 1 WHERE ID = :id');
-$rqt->bindValue(":id", $id);
-$rqt->execute();
-*/
 
 if($_POST['id'])
 {
